974d9e0fcfe92ef0f9c51976cf64bdea65f2e118
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-15 15:34:19 +0100

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
 console.log("test");
+
+
+/* 
+
+function
+    beim start zahlen runter zählen
+    
+
+
+
+window.onload
+setInterval()
+clearInterval()
+getElementsByClassName()/getElementById()
+if, else if
+*/
