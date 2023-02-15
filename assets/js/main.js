4157083f5dd30b5251f830ec68f9ba8e1a5a7e22
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-15 16:32:39 +0100

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ console.log("test");
 
 function
     beim start zahlen runter zählen
-    
+
 
 
 
@@ -15,3 +15,41 @@ clearInterval()
 getElementsByClassName()/getElementById()
 if, else if
 */
+
+const sectionDiv =  document.querySelector(`.message`)
+
+
+// erst nachdem Design und alles geladen ist ausführen
+window.onload = functionAllesIstDa;
+
+
+function functionAllesIstDa() {
+    console.log("in functionAllesIstDa");
+
+    // counter
+    let counter = 10;
+    
+    // setInterval()
+    const stopInterval = setInterval(() => {
+        counter = counter - 1;
+
+        // ins html
+        // output holen
+        const outputField = document.querySelector("#count");
+        outputField.innerHTML = counter;
+
+        if(counter == 0){
+            // window.location.reload();   // würde Seite neu laden alles beginnt von vorne
+            clearInterval(stopInterval);
+            document.querySelector("div").classList.remove("message");  // Style und Kreis gehen weg
+
+            document.querySelector("p" ).style.display = "none";    // langer Text geht weg
+
+           sectionDiv.style.display = "none";    // alles weg
+           // wenn const sectionDiv  --> global variable ist, dann gehts
+
+        }
+
+    },1000);
+
+};
